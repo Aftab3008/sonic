6393fb8ce6f2bf637409f7c62b828f1cd73689d7
Aftab3008 <shaikaftab005@gmail.com>
2026-05-24 21:58:07 +0530

    Fix: iOS building issue 2

--- apps/sonic-kmp/iosApp/iosApp/iOSApp.swift
+++ apps/sonic-kmp/iosApp/iosApp/iOSApp.swift
@@ -3,6 +3,8 @@ import ComposeApp
 
 @main
 struct iOSApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
     @Environment(\.scenePhase) private var scenePhase
 
     var body: some Scene {
@@ -21,8 +23,4 @@ class AppDelegate: NSObject, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         MainViewControllerKt.onAppTerminate()
     }
-}
-
-extension iOSApp {
-    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
 }
